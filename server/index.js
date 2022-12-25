d4c87dd021b9e860428d515a6ec1c125a6925a3a
MirHeyder03 <mhyahyayev03@gmail.com>
2022-12-25 22:20:58 +0400

    backend : authorizatuon added,token,localstore

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
 import noteRoute from "./routes/notes.js";
+import userRoute from "./routes/users.js";
 import con from "./db/dbConnect.js";
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 
 //!routes
 app.use("/api/v1/notes", noteRoute);
+app.use("/api/v1/user", userRoute);
 
 app.listen(process.env.PORT, () => {
   console.log("listen on port");
